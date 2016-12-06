a684463fcc524e6bd635ba4a06fcd150b495893e
paryyongwoo <apfhd5620@gmail.com>
2016-12-06 12:37:19 +0900

    ProducerDTO 내용추가, UserProductMapper 내용추가 (박용우)
    ProducerDTO 내용추가, UserProductMapper 내용추가 (박용우)

--- src/main/java/spring/web/controller/UserProductController.java
+++ src/main/java/spring/web/controller/UserProductController.java
@@ -53,11 +53,6 @@ public class UserProductController {
 		/**
 		 * 1. 사용자가 선택한 카테고리를 인수로 받음
 		 * 2. 받은 카테고리를 이용해서 dao로 가서 등록순으로 List<ProductDTO>에 저장 후 뷰로 반환해줌
-		 * 
-		 * categoryNo
-		 * 1 - 과일/견과
-		 * 2 - 채소
-		 * 3 - 쌀/잡곡
 		 * */
 		
 		List<ProductDTO> list = null;
--- src/main/java/spring/web/dto/ProducerDTO.java
+++ src/main/java/spring/web/dto/ProducerDTO.java
@@ -1,5 +1,64 @@
 package spring.web.dto;
 
 public class ProducerDTO {
+	private int no;
+	private String name;
+	private String addr;
+	private String phone;
+	private String registerDate;
+	
+	public ProducerDTO() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public ProducerDTO(int no, String name, String addr, String phone, String registerDate) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.addr = addr;
+		this.phone = phone;
+		this.registerDate = registerDate;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getRegisterDate() {
+		return registerDate;
+	}
+
+	public void setRegisterDate(String registerDate) {
+		this.registerDate = registerDate;
+	}
+	
+	
 }
