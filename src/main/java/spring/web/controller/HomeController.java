8f7c3776be138a467bebf4948f6fb92fc34b854d
paryyongwoo <apfhd5620@gmail.com>
2016-12-07 17:17:50 +0900

    ㅁㅁ
    ㅁ

--- src/main/java/spring/web/controller/HomeController.java
+++ src/main/java/spring/web/controller/HomeController.java
@@ -3,9 +3,6 @@ package spring.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
